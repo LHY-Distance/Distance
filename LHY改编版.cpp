7f6f26fb36d61d9400531f5e87d65ab0f48e2ced
LHY-Distance <72370135+LHY-Distance@users.noreply.github.com>
2020-10-10 16:59:14 +0800

    Add files via upload

---
+++ LHY改编版.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+	
+	cout<<"                                                                         \n";
+	cout<<"                                                                         \n";
+	cout<<"                                                                         \n";
+	cout<<"                                                                         \n";
+	cout<<"           込込              込込        込込    込込            込込    \n";
+	cout<<"           込込              込込        込込      込込        込込      \n";
+	cout<<"           込込              込込        込込        込込    込込        \n";
+	cout<<"           込込              込込        込込          込込込込          \n";
+	cout<<"           込込              込込込込込込込込            込込            \n";
+	cout<<"           込込              込込        込込            込込            \n";
+	cout<<"           込込              込込        込込            込込            \n";
+	cout<<"           込込込込込込込    込込        込込            込込            \n";
+	cout<<"           込込込込込込込    込込        込込            込込            \n";
+	cout<<"                                                                         \n";
+	cout<<"                                                                         \n";
+	cout<<"                                                                         \n";
+	cout<<"                                                                         \n";
+	system("pause"); 
+	return 0;
+}
+ 
---
+++ a+b.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	int a,b,sum;
+	cin>>a>>b;
+	sum=a+b;
+	cout<<sum;
+	system("pause") ;
+	return 0;
+ } 
